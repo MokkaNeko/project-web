7afa5208df3f79a1c142a0a34b5ebd9a48935693
Iksanard <l200210051@student.ums.ac.id>
2023-07-20 11:18:17 +0700

    re

--- login/php/config.php
+++ login/php/config.php
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 <?php
 $con = mysqli_connect("localhost", "root", "", "class_system")
-=======
-<?php
-$con = mysqli_connect("localhost", "root", "", "class_system")
->>>>>>> f81c3210d9b0d64a41415c9d9e157b03f15f42d1
 ?>
